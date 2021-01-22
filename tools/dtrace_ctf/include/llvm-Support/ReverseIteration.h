7eb8fdb00f1dc10230932f829df31e471a60d480
William Kent <wjk011@gmail.com>
2021-01-21 22:03:07 -0500

    Add ctfconvert to the build

---
+++ tools/dtrace_ctf/include/llvm-Support/ReverseIteration.h
@@ -0,0 +1,18 @@
+#ifndef LLVM_SUPPORT_REVERSEITERATION_H
+#define LLVM_SUPPORT_REVERSEITERATION_H
+
+#include "llvm-Support/PointerLikeTypeTraits.h"
+
+namespace llvm {
+
+template<class T = void *>
+bool shouldReverseIterate() {
+#if LLVM_ENABLE_REVERSE_ITERATION
+  return detail::IsPointerLike<T>::value;
+#else
+  return false;
+#endif
+}
+
+}
+#endif
