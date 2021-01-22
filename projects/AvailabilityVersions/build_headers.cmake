587c39bdd4924280adb12300843d2e4dea4db214
William Kent <wjk011@gmail.com>
2021-01-22 16:05:11 -0500

    Fix AvailabilityHeaders generation command

--- projects/AvailabilityVersions/build_headers.cmake
+++ projects/AvailabilityVersions/build_headers.cmake
@@ -1,9 +1,11 @@
+file(MAKE_DIRECTORY ${HEADER_DIR}/dyld)
+
 execute_process(
-    COMMAND ./build_version_map.rb availability.pl
-    OUTPUT_FILE $ENV{OUT_DIR}/dyld/VersionMap.h
+    COMMAND ${SOURCE_DIR}/build_version_map.rb ${SOURCE_DIR}/availability.pl
+    OUTPUT_FILE ${HEADER_DIR}/dyld/VersionMap.h
 )
 
 execute_process(
-    COMMAND ./print_dyld_os_versions.rb availability.pl
-    OUTPUT_FILE $ENV{OUT_DIR}/dyld/for_dyld_priv.inc
+    COMMAND ${SOURCE_DIR}/print_dyld_os_versions.rb ${SOURCE_DIR}/availability.pl
+    OUTPUT_FILE ${HEADER_DIR}/dyld/for_dyld_priv.inc
 )
