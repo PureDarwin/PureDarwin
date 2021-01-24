002a4e69a6cac916fa8d7e03d54f996a8d69679c
William Kent <wjk011@gmail.com>
2021-01-23 19:20:16 -0500

    Initial commit of xnu_headers target

---
+++ projects/xnu/preprocess_files.cmake
@@ -0,0 +1,2 @@
+configure_file(${XNU_SRC}/makedefs/MakeInc.cmd.in ${XNU_SRC}/makedefs/MakeInc.cmd @ONLY)
+configure_file(${XNU_SRC}/bsd/sys/make_symbol_aliasing.sh.in ${XNU_SRC}/bsd/sys/make_symbol_aliasing.sh @ONLY)
