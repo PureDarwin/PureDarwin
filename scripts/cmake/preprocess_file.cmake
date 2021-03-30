26aa95fe5d73d6e684bfe22ddd4ff5dc99490d6a
William Kent <wjk011@gmail.com>
2021-03-29 21:49:53 -0400

    Update preprocess_file()

--- scripts/cmake/preprocess_file.cmake
+++ scripts/cmake/preprocess_file.cmake
@@ -5,19 +5,19 @@ macro(preprocess_file input output)
     endforeach()
 
     if(NOT IS_ABSOLUTE input)
-        set(input2 ${CMAKE_CURRENT_SOURCE_DIR}/${input})
+        set(real_input ${CMAKE_CURRENT_SOURCE_DIR}/${input})
     else()
-        set(input2 ${input})
+        set(real_input ${input})
     endif()
 
     add_custom_command(
         OUTPUT ${output}
-        COMMAND ${CMAKE_COMMAND} -D INPUT=${input2} -D OUTPUT=${output} ${flags} -P ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake
-        DEPENDS ${input2} ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake
+        COMMAND ${CMAKE_COMMAND} -D INPUT=${real_input} -D OUTPUT=${output} ${flags} -P ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake
+        DEPENDS ${real_input} ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake
         COMMENT "Configure ${output}" VERBATIM
     )
 
     # Unset our private variables before leaving macro scope
     set(flags)
-    set(input2)
+    set(real_input)
 endmacro()
