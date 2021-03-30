e88a517ef6ff2f237d3be5ca7f2940d596101211
William Kent <wjk011@gmail.com>
2021-03-29 21:35:32 -0400

    Fix preprocess_file()

--- scripts/cmake/preprocess_file.cmake
+++ scripts/cmake/preprocess_file.cmake
@@ -1,20 +1,23 @@
 macro(preprocess_file input output)
     set(flags)
-    foreach(arg IN LISTS ARGN)
+    foreach(arg ${ARGN})
         list(APPEND flags -D ${arg})
     endforeach()
 
     if(NOT IS_ABSOLUTE input)
-        set(input ${input} ${CMAKE_CURRENT_SOURCE_DIR}/${input})
+        set(input2 ${CMAKE_CURRENT_SOURCE_DIR}/${input})
+    else()
+        set(input2 ${input})
     endif()
 
     add_custom_command(
         OUTPUT ${output}
-        COMMAND ${CMAKE_COMMAND} -P ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake -D INPUT=${input} -D OUTPUT=${output} ${flags}
-        DEPENDS ${input}
+        COMMAND ${CMAKE_COMMAND} -D INPUT=${input2} -D OUTPUT=${output} ${flags} -P ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake
+        DEPENDS ${input2} ${PUREDARWIN_SOURCE_DIR}/scripts/cmake/preprocess_file_script.cmake
         COMMENT "Configure ${output}" VERBATIM
     )
 
     # Unset our private variables before leaving macro scope
     set(flags)
+    set(input2)
 endmacro()
