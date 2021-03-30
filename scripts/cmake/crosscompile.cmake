5b8deb2c8836fe3c412fd9bf0ce310fbe549fb52
William Kent <wjk011@gmail.com>
2021-03-29 20:16:20 -0400

    Add add_darwin_shared_library() function

--- scripts/cmake/crosscompile.cmake
+++ scripts/cmake/crosscompile.cmake
@@ -10,3 +10,24 @@ function(add_darwin_static_library name)
     target_compile_options(${name} PRIVATE -target x86_64-apple-darwin20)
     target_compile_options(${name} PRIVATE -nostdlib -nostdinc)
 endfunction()
+
+function(add_darwin_shared_library name)
+    cmake_parse_arguments(SL "" "MACOSX_VERSION_MIN" "" ${ARGN})
+
+    add_library(${name} SHARED)
+    add_dependencies(${name} darwin_ld)
+    target_link_options(${name} PRIVATE -fuse-ld=$<TARGET_FILE:darwin_ld>)
+
+    string(SUBSTRING ${name} 0 3 name_prefix)
+    if(name_prefix STREQUAL "lib")
+        set_property(TARGET ${name} PROPERTY PREFIX "")
+    endif()
+
+    target_compile_options(${name} PRIVATE -target x86_64-apple-darwin20)
+    target_compile_options(${name} PRIVATE -nostdlib -nostdinc)
+
+    if(SL_MACOSX_VERSION_MIN)
+        target_compile_options(${name} PRIVATE -mmacosx-version-min=${SL_MACOSX_VERSION_MIN})
+        target_link_options(${name} PRIVATE -mmacosx-version-min=${SL_MACOSX_VERSION_MIN})
+    endif()
+endfunction()
