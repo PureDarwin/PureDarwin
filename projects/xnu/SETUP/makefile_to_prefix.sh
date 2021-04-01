9c9d77632704cbb81eb81410a69120b785fbc6a1
William Kent <wjk011@gmail.com>
2021-04-01 12:57:29 -0400

    Update xnu.bsd_conf target generation
    Also updated the header include order for xnu.bsd_module.

--- projects/xnu/SETUP/makefile_to_prefix.sh
+++ projects/xnu/SETUP/makefile_to_prefix.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-cat $INPUT | fgrep CONFIG_DEFINES | sed -Ee 's,^export CONFIG_DEFINES = ,,' -e 's,-D,\
-#define ,g' -e 's,=, ,g' -e 's,",,g' > $OUTPUT
+if [ -z "$1" -o -z "$2" ]; then
+    echo Two arguments must be provided. 1>&2
+    exit 1
+fi
+
+cat $1 | fgrep CONFIG_DEFINES | sed -Ee 's,^export CONFIG_DEFINES = ,,' -e 's,-D,\
+#define ,g' -e 's,=, ,g' -e 's,",,g' > $2
