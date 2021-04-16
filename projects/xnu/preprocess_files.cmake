b81e128131e2605fabbcdc0cf8e0a8de5510c36d
William Kent <wjk011@gmail.com>
2021-04-16 18:20:45 -0400

    Remove unused file

--- projects/xnu/preprocess_files.cmake
+++
@@ -1,2 +0,0 @@
-configure_file(${XNU_SRC}/makedefs/MakeInc.cmd.in ${XNU_SRC}/makedefs/MakeInc.cmd @ONLY)
-configure_file(${XNU_SRC}/bsd/sys/make_symbol_aliasing.sh.in ${XNU_SRC}/bsd/sys/make_symbol_aliasing.sh @ONLY)
