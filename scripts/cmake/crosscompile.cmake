8665f1a21b7319cc7dfd9d9b05e77bfa91c3960e
William Kent <wjk011@gmail.com>
2021-04-16 16:12:25 -0400

    Add add_darwin_executable()

--- scripts/cmake/crosscompile.cmake
+++ scripts/cmake/crosscompile.cmake
@@ -1,3 +1,20 @@
+function(add_darwin_executable name)
+    cmake_parse_arguments(SL "NO_STANDARD_LIBRARIES" "MACOSX_VERSION_MIN" "" ${ARGN})
+
+    add_executable(${name})
+    add_dependencies(${name} darwin_ld)
+    target_link_options(${name} PRIVATE -fuse-ld=$<TARGET_FILE:darwin_ld>)
+    target_compile_options(${name} PRIVATE -target x86_64-apple-darwin20)
+    target_compile_options(${name} PRIVATE -nostdlib -nostdinc)
+
+    # TODO: Handle SL_NO_STANDARD_LIBRARIES here, once the libraries have been added to the build.
+
+    if(SL_MACOSX_VERSION_MIN)
+        target_compile_options(${name} PRIVATE -mmacosx-version-min=${SL_MACOSX_VERSION_MIN})
+        target_link_options(${name} PRIVATE -mmacosx-version-min=${SL_MACOSX_VERSION_MIN})
+    endif()
+endfunction()
+
 function(add_darwin_static_library name)
     add_library(${name} STATIC)
     add_dependencies(${name} darwin_libtool)
