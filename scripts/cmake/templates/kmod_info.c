00cf6ffb19b7c4104c056359531dd5f4ac3bac69
William Kent <wjk011@gmail.com>
2021-07-03 21:11:40 -0400

    Create add_kmod_info() function

---
+++ scripts/cmake/templates/kmod_info.c
@@ -0,0 +1,9 @@
+#include <mach/kmod.h>
+
+extern kern_return_t _start(kmod_info_t *ki, void *data);
+extern kern_return_t _stop(kmod_info_t *ki, void *data);
+
+__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(@KEXT_IDENTIFIER@, "@KEXT_VERSION@", _start, _stop)
+__private_extern__ kmod_start_func_t *_realmain = @KEXT_REALMAIN@;
+__private_extern__ kmod_stop_func_t *_antimain = @KEXT_ANTIMAIN@;
+__private_extern__ int _kext_apple_cc = __APPLE_CC__;
