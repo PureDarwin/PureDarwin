0446295c6dc9a0ae5de2e9b43bacf92beece4f56
Tyler Besselman <beeselmane@gmail.com>
2022-05-09 12:46:22 -0400

    Add submodules
    Note: The dbuild submodule currently points to my own fork of PureDarwin/dbuild.
    This will need to be fixed if this is merged back to PureDarwin/PureDarwin

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_PROCESSOR x86_64) # TODO: arm64 support
 
-set(triple x86_64-apple-macos${CMAKE_MACOSX_MIN_VERSION})
+set(triple x86_64-apple-darwin${RC_DARWIN_VERSION})
 
 set(CMAKE_C_COMPILER ${RC_HOST_BIN}/clang)
 set(CMAKE_C_COMPILER_TARGET ${triple})
