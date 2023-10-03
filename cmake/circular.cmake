5a04e98aa375118c32ebefbba2845a1dd8579d3f
William Kent <wjk011@gmail.com>
2023-10-03 12:19:29 -0400

    Fix important bug in circular.cmake

--- cmake/circular.cmake
+++ cmake/circular.cmake
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: GPL-3
 
 function(add_darwin_circular_library name)
-    cmake_parse_arguments(CIRCULAR "" "INSTALL_NAME_DIR" "OBJECT_LIBRARIES;SIBLINGS;STRONG_SIBLINGS;DEPENDENCIES;UPWARD;STRONG_DEPENDENCIES" ${ARGN})
+    cmake_parse_arguments(CIRCULAR "" "INSTALL_NAME" "OBJECT_LIBRARIES;SIBLINGS;STRONG_SIBLINGS;DEPENDENCIES;UPWARD;STRONG_DEPENDENCIES" ${ARGN})
 
     if(NOT CIRCULAR_OBJECT_LIBRARIES)
         message(FATAL_ERROR "Need to specify at least one value under OBJECT_LIBRARIES")
     endif()
 
     set(firstpass_name "${name}_firstpass")
-    add_darwin_shared_library(${firstpass_name} INSTALL_NAME_DIR ${CIRCULAR_INSTALL_NAME_DIR})
+    add_darwin_shared_library(${firstpass_name} INSTALL_NAME ${CIRCULAR_INSTALL_NAME})
     set_property(${firstpass_name} PROPERTY OUTPUT_NAME ${name})
     set_property(${firstpass_name} PROPERTY SUFFIX .firstpass_dylib)
 
@@ -26,7 +26,7 @@ function(add_darwin_circular_library name)
 
     target_link_libraries(${fistpass_name} PRIVATE ${CIRCULAR_STRONG_DEPENDENCIES})
 
-    add_darwin_shared_library(${name} INSTALL_NAME_DIR ${CIRCULAR_INSTALL_NAME_DIR})
+    add_darwin_shared_library(${name} INSTALL_NAME ${CIRCULAR_INSTALL_NAME})
     foreach(lib ${CIRCULAR_OBJECT_LIBRARIES})
         target_sources(${name} PRIVATE $<TARGET_OBJECTS:${lib}>)
     endforeach()
