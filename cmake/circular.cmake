6c4ce990569fec9fa7a8ad57254dab0175ea3602
William Kent <wjk011@gmail.com>
2024-09-11 11:54:41 -0400

    Change add_darwin_circular_library to allow custom output name

--- cmake/circular.cmake
+++ cmake/circular.cmake
@@ -3,15 +3,21 @@
 # SPDX-License-Identifier: GPL-3
 
 function(add_darwin_circular_library name)
-    cmake_parse_arguments(CIRCULAR "" "INSTALL_NAME" "OBJECT_LIBRARIES;SIBLINGS;STRONG_SIBLINGS;DEPENDENCIES;UPWARD;STRONG_DEPENDENCIES" ${ARGN})
+    cmake_parse_arguments(CIRCULAR "" "INSTALL_NAME;OUTPUT_NAME" "OBJECT_LIBRARIES;SIBLINGS;STRONG_SIBLINGS;DEPENDENCIES;UPWARD;STRONG_DEPENDENCIES" ${ARGN})
 
     if(NOT CIRCULAR_OBJECT_LIBRARIES)
         message(FATAL_ERROR "Need to specify at least one value under OBJECT_LIBRARIES")
     endif()
 
+    if(CIRCULAR_OUTPUT_NAME)
+        set(output_name ${CIRCULAR_OUTPUT_NAME})
+    else()
+        set(output_name ${name})
+    endif()
+
     set(firstpass_name "${name}_firstpass")
     add_darwin_shared_library(${firstpass_name} INSTALL_NAME ${CIRCULAR_INSTALL_NAME})
-    set_property(TARGET ${firstpass_name} PROPERTY OUTPUT_NAME ${name})
+    set_property(TARGET ${firstpass_name} PROPERTY OUTPUT_NAME ${output_name})
     set_property(TARGET ${firstpass_name} PROPERTY SUFFIX .firstpass_dylib)
 
     foreach(lib ${CIRCULAR_OBJECT_LIBRARIES})
@@ -27,6 +33,8 @@ function(add_darwin_circular_library name)
     target_link_libraries(${firstpass_name} PRIVATE ${CIRCULAR_STRONG_DEPENDENCIES})
 
     add_darwin_shared_library(${name} INSTALL_NAME ${CIRCULAR_INSTALL_NAME})
+    set_property(TARGET ${name} PROPERTY OUTPUT_NAME ${output_name})
+
     foreach(lib ${CIRCULAR_OBJECT_LIBRARIES})
         target_sources(${name} PRIVATE $<TARGET_OBJECTS:${lib}>)
     endforeach()
